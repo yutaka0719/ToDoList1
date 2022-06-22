697270e942f1cf83e6bee99a862ce1332cdf20b4
yutaka0719 <45693509+yutaka0719@users.noreply.github.com>
2022-06-22 22:15:05 +0900

    js作成４

--- src/index.js
+++ src/index.js
@@ -16,9 +16,23 @@ const onClickAdd = () => {
   //button(完了)タグ追加
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button(削除)タグ追加
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグ(div)を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTaeget);
+  });
 
   //divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
